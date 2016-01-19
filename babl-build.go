cbad1687208cf58b463cee45c8fea6760d68fcb1
Lars Kluge <l@larskluge.com>
2016-01-19 14:18:02 +0100

    .

---
+++ babl-build.go
@@ -0,0 +1,313 @@
+package main
+
+import (
+	"bytes"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/exec"
+	"os/user"
+	"path/filepath"
+	"regexp"
+	"sort"
+	"strings"
+
+	"gopkg.in/yaml.v2"
+)
+
+type config struct {
+	Id        string `yaml:"id" json:"id"`
+	Container struct {
+		Type   string `yaml:"type" json:"type"`
+		Docker struct {
+			Image          string `yaml:"image" json:"image"`
+			ForcePullImage bool   `yaml:"forcePullImage" json:"forcePullImage"`
+			Network        string `yaml:"network" json:"network"`
+		} `yaml:"docker" json:"docker"`
+		Options []string `yaml:"options" json:"options"`
+	} `yaml:"container" json:"container"`
+	Instances int      `yaml:"instances" json:"instances"`
+	Cpus      float64  `yaml:"cpus" json:"cpus"`
+	Mem       float64  `yaml:"mem" json:"mem"`
+	Uris      []string `yaml:"uris" json:"uris"`
+	Env       struct {
+		ServiceTags string `yaml:"SERVICE_TAGS" json:"SERVICE_TAGS"`
+		BablModule  string `yaml:"BABL_MODULE" json:"BABL_MODULE"`
+		BablCommand string `yaml:"BABL_COMMAND" json:"BABL_COMMAND"`
+	} `yaml:"env" json:"env"`
+	Cmd string `yaml:"cmd" json:"cmd"`
+}
+
+var (
+	// options
+	dryRun                   bool
+	configFile, marathonHost string
+
+	// YAML "overwrites"
+	overwrites config
+)
+
+// auxiliary functions
+
+func getOutput(cmd string, args ...string) string {
+	output, err := exec.Command(cmd, args...).Output()
+	if err != nil {
+		panic(err)
+	}
+	return string(output)
+}
+
+func homeDir() string {
+	u, err := user.Current()
+	if err != nil {
+		panic(err)
+	}
+	return u.HomeDir
+}
+
+// non-command functions
+
+func module() string {
+	dir, err := os.Getwd()
+	if err != nil {
+		panic(err)
+	}
+	return filepath.Base(dir)
+}
+
+func id() string {
+	return fmt.Sprintf("%s-%s", _type(), module())
+}
+
+func _type() string {
+	if tags := overwrites.Env.ServiceTags; tags != "" {
+		return tags
+	}
+	return "babl"
+}
+
+func containerOptions() []string {
+	if opts := overwrites.Container.Options; opts != nil {
+		return opts
+	}
+	return []string{}
+}
+
+func version() string {
+	return "v" + strings.TrimSpace(getOutput(
+		"git", "rev-list", "HEAD", "--count"))
+}
+
+func image() string {
+	return fmt.Sprintf("registry.babl.sh:5000/%s:%s", id(), version())
+}
+
+func imageLatest() string {
+	return regexp.MustCompile(":[^:]+$").ReplaceAllString(image(), ":latest")
+}
+
+func conf() config {
+	var c config
+	contents, err := ioutil.ReadFile(configFile)
+	if err != nil {
+		panic(err)
+	}
+	if err := yaml.Unmarshal(contents, &c); err != nil {
+		panic(err)
+	}
+	contents, err = ioutil.ReadFile(".babl-build.yml")
+	if err != nil {
+		panic(err)
+	}
+	if err := yaml.Unmarshal(contents, &c); err != nil {
+		panic(err)
+	}
+	c.Id = id()
+	c.Container.Docker.Image = image()
+	c.Env.BablModule = module()
+	return c
+}
+
+func execute(cmd string, args ...string) {
+	fmt.Println(cmd + " " + strings.Join(args, " "))
+	if !dryRun {
+		if err := exec.Command(cmd, args...).Run(); err != nil {
+			os.Exit(1)
+		}
+	}
+}
+
+type command struct {
+	Desc string
+	Func func(...string)
+}
+
+var commands map[string]command
+
+func init() {
+	// init overwrites
+	contents, err := ioutil.ReadFile(".babl-build.yml")
+	if err == nil {
+		_ = yaml.Unmarshal(contents, &overwrites) // ignore error
+	}
+
+	// init commands
+	commands = map[string]command{
+		"build": {
+			"Build Docker image",
+			func(args ...string) {
+				cmd := append([]string{"build", "-t", image()}, args...)
+				cmd = append(cmd, ".")
+				execute("docker", cmd...)
+				execute("docker", "tag", "-f", image(), imageLatest())
+			},
+		},
+		"version": {
+			"Print the current version to be deployed",
+			func(args ...string) {
+				fmt.Println(version())
+			},
+		},
+		"config": {
+			"Print the Marathon JSON config",
+			func(args ...string) {
+				err := json.NewEncoder(os.Stdout).Encode(conf())
+				if err != nil {
+					panic(err)
+				}
+			},
+		},
+		"push": {
+			"Push Docker image to remote registry",
+			func(args ...string) {
+				execute("docker", "push", image())
+				execute("docker", "push", imageLatest())
+			},
+		},
+		"deploy": {
+			"Deploy a Babl module",
+			func(args ...string) {
+				body := bytes.NewBuffer([]byte{})
+				if err := json.NewEncoder(body).Encode(conf()); err != nil {
+					panic(err)
+				}
+				req, err := http.NewRequest("POST",
+					fmt.Sprintf("http://%s:8080/v2/apps", marathonHost), body)
+				if err != nil {
+					panic(err)
+				}
+				req.Header.Set("Content-Type", "application/json")
+
+				resp, err := (&http.Client{}).Do(req)
+				if err != nil {
+					panic(err)
+				}
+
+				if resp.StatusCode >= 200 && resp.StatusCode < 400 {
+					_, _ = io.Copy(os.Stdout, resp.Body) // ignore error
+				}
+				_ = resp.Body.Close() // ignore error
+			},
+		},
+		"destroy": {
+			"Destroy a Babl module",
+			func(args ...string) {
+				execute("curl", "-s", "-X", "DELETE",
+					fmt.Sprintf("'http://%s:8080/v2/apps/%s'",
+						marathonHost, id()))
+			},
+		},
+		"dist": {
+			"build & push & destroy & deploy",
+			func(args ...string) {
+				commands["build"].Func()
+				commands["push"].Func()
+				commands["destroy"].Func()
+				commands["deploy"].Func()
+			},
+		},
+		"play": {
+			"Play (run) a local built Babl module",
+			func(args ...string) {
+				execArgs := []string{"docker", "run", "-it", "--rm", "-p",
+					"4444:4444", "-e", "BABL_MODULE=" + module(), "-e",
+					"BABL_COMMAND=/bin/app"}
+				execArgs = append(execArgs, containerOptions()...)
+				execArgs = append(execArgs, image())
+				execute(execArgs[0], execArgs[1:]...)
+			},
+		},
+		"sh": {
+			"Run the container with a shell",
+			func(args ...string) {
+				execArgs := []string{"docker", "run", "-it", "--rm", "-p",
+					"4444:4444", "-e", "BABL_MODULE=" + module(), "-e",
+					"BABL_COMMAND=/bin/app"}
+				execArgs = append(execArgs, containerOptions()...)
+				execArgs = append(execArgs, image(), "sh")
+				execute(execArgs[0], execArgs[1:]...)
+			},
+		},
+		"help": {
+			"Describe available commands",
+			func(args ...string) {
+				fmt.Fprintln(os.Stderr, "Commands:")
+				i, maxLen, names := 0, 0, make([]string, len(commands))
+				for name := range commands {
+					names[i] = name
+					if len(name) > maxLen {
+						maxLen = len(name)
+					}
+					i++
+				}
+				sort.Sort(sort.StringSlice(names))
+				formatString := fmt.Sprintf("  %%s %%-%ds  # %%s\n", maxLen)
+				for _, name := range names {
+					fmt.Fprintf(os.Stderr, formatString, os.Args[0], name,
+						commands[name].Desc)
+				}
+				fmt.Fprintln(os.Stderr)
+
+				fmt.Fprintln(os.Stderr, "Options:")
+				maxLen = 0
+				flag.VisitAll(func(f *flag.Flag) {
+					if len(f.Name)+4 > maxLen {
+						maxLen = len(f.Name) + 4
+					}
+				})
+				formatString = fmt.Sprintf("  %%-%ds  # Default: %%s\n",
+					maxLen)
+				flag.VisitAll(func(f *flag.Flag) {
+					fmt.Fprintf(os.Stderr, formatString, "[--"+f.Name+"]",
+						f.DefValue)
+				})
+				fmt.Fprintln(os.Stderr)
+			},
+		},
+	}
+
+	// init options
+	flag.BoolVar(&dryRun, "dry-run", false, "")
+	flag.StringVar(&configFile, "config-file",
+		homeDir()+"/.babl-build.yml", "")
+	flag.StringVar(&marathonHost, "marathon-host", "127.0.0.1", "")
+	flag.Usage = func() {
+		commands["help"].Func()
+	}
+}
+
+func main() {
+	flag.Parse()
+
+	if len(flag.Args()) == 0 {
+		commands["help"].Func()
+	} else if cmd, ok := commands[flag.Arg(0)]; ok {
+		cmd.Func(flag.Args()...)
+	} else {
+		fmt.Fprintf(os.Stderr, "Could not find command \"%s\".\n", flag.Arg(0))
+	}
+}
