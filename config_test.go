5a1a3fdcb28d42c1b01fe8f85ff33d903351ed07
Lars Kluge <l@larskluge.com>
2016-01-22 02:09:04 +0100

    Modify tests for desired namespace’d module names

--- config_test.go
+++ config_test.go
@@ -8,45 +8,32 @@ import (
 	"testing"
 )
 
-const testModuleDir = "test-modules"
+const testModuleDir = "test-modules/larskluge"
 
 func TestConfig(t *testing.T) {
 	wd, err := os.Getwd()
 	if err != nil {
 		panic(err)
 	}
-	err = filepath.Walk(filepath.Join(wd, testModuleDir),
-		func(path string, info os.FileInfo, err error) error {
-			// only test in module dirs
-			if !info.IsDir() || filepath.Base(path) == testModuleDir {
-				return nil
-			}
-			if filepath.Base(path) == ".git" {
-				return filepath.SkipDir
-			}
+	modules, _ := filepath.Glob(filepath.Join(wd, testModuleDir, "*"))
+	for _, path := range modules {
+		// cd to the module dir
+		if err := os.Chdir(path); err != nil {
+			panic(err)
+		}
 
-			// cd to the module dir
-			if err := os.Chdir(path); err != nil {
-				panic(err)
-			}
-
-			// compare generated JSON to expected JSON
-			contents, err := ioutil.ReadFile("expected-config.json")
-			if err != nil {
-				panic(err)
-			}
-			var buf bytes.Buffer
-			stdout = &buf
-			commands["config"].Func()
-			stdout = os.Stdout
-			if !bytes.Equal(contents, buf.Bytes()) {
-				t.Errorf("config mismatch: want %s; got %s",
-					string(contents), string(buf.Bytes()))
-			}
-
-			return nil
-		})
-	if err != nil {
-		panic(err)
+		// compare generated JSON to expected JSON
+		contents, err := ioutil.ReadFile("expected-config.json")
+		if err != nil {
+			panic(err)
+		}
+		var buf bytes.Buffer
+		stdout = &buf
+		commands["config"].Func()
+		stdout = os.Stdout
+		if !bytes.Equal(contents, buf.Bytes()) {
+			t.Errorf("config mismatch: want %s; got %s",
+				string(contents), string(buf.Bytes()))
+		}
 	}
 }
