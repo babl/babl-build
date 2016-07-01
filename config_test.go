fbb46141e7bd8286b73ce1dd0934a4c09752a85a
Lars Kluge <l@larskluge.com>
2016-07-01 04:14:23 +0100

    Add a real tests

--- config_test.go
+++ config_test.go
@@ -1,35 +1,95 @@
 package main
 
 import (
-	"bytes"
-	"io/ioutil"
-	"os"
-	"path/filepath"
 	"testing"
 )
 
-func TestConfig(t *testing.T) {
-	modules, _ := filepath.Glob(filepath.Join(testModuleDir(), "*"))
-	for _, path := range modules {
-		t.Logf("Check %s", path)
-		_conf = nil // clear conf() cache
-		// cd to the module dir
-		if err := os.Chdir(path); err != nil {
-			panic(err)
-		}
+func TestModuleId(t *testing.T) {
+	c := execConfigParsed("string-upcase")
+	expected := "larskluge/string-upcase"
+	actual := c.Id
+	if expected != actual {
+		t.Errorf("config mismatch: want %s; got %s", expected, actual)
+	}
+}
 
-		// compare generated JSON to expected JSON
-		contents, err := ioutil.ReadFile("expected-config.json")
-		if err != nil {
-			panic(err)
-		}
-		var buf bytes.Buffer
-		stdout = &buf
-		commands["config"].Func()
-		stdout = os.Stdout
-		if !bytes.Equal(contents, buf.Bytes()) {
-			t.Errorf("config mismatch: want %s; got %s",
-				string(contents), string(buf.Bytes()))
-		}
+func TestCustomizedMem(t *testing.T) {
+	c := execConfigParsed("image-resize")
+	expected := 128.0
+	actual := *c.Mem
+	if expected != actual {
+		t.Errorf("config mismatch: want %s; got %s", expected, actual)
+	}
+}
+
+func TestDefaultMem(t *testing.T) {
+	c := execConfigParsed("string-upcase")
+	expected := 16.0
+	actual := *c.Mem
+	if expected != actual {
+		t.Errorf("config mismatch: want %s; got %s", expected, actual)
+	}
+}
+
+func TestUnlimitedMem(t *testing.T) {
+	c := execConfigParsed("unlimited-mem")
+	expected := 0.0
+	actual := *c.Mem
+	if expected != actual {
+		t.Errorf("config mismatch: want %s; got %s", expected, actual)
+	}
+}
+
+func TestCustomizedCpus(t *testing.T) {
+	c := execConfigParsed("image-resize")
+	expected := 0.6
+	actual := c.Cpus
+	if expected != actual {
+		t.Errorf("config mismatch: want %s; got %s", expected, actual)
+	}
+}
+
+func TestKafkaBrokers(t *testing.T) {
+	c := execConfigParsed("string-upcase")
+	expected := "queue.babl.sh:9092"
+	actual := c.Env.BablKafkaBrokers
+	if expected != actual {
+		t.Errorf("config mismatch: want %s; got %s", expected, actual)
+	}
+}
+
+func TestDefaultDockerForcePullDisabled(t *testing.T) {
+	c := execConfigParsed("string-upcase")
+	expected := false
+	actual := c.Container.Docker.ForcePullImage
+	if expected != actual {
+		t.Errorf("config mismatch: want %s; got %s", expected, actual)
+	}
+}
+
+func TestDefaultNetwork(t *testing.T) {
+	c := execConfigParsed("string-upcase")
+	expected := "BRIDGE"
+	actual := c.Container.Docker.Network
+	if expected != actual {
+		t.Errorf("config mismatch: want %s; got %s", expected, actual)
+	}
+}
+
+func TestDockerImage(t *testing.T) {
+	c := execConfigParsed("string-upcase")
+	expected := "registry.babl.sh/larskluge/string-upcase:v20"
+	actual := c.Container.Docker.Image
+	if expected != actual {
+		t.Errorf("config mismatch: want %s; got %s", expected, actual)
+	}
+}
+
+func TestVolumeMounts(t *testing.T) {
+	c := execConfigParsed("babl-build")
+	expected := "/usr/lib64/libsystemd.so.0"
+	actual := c.Container.Volumes[4].HostPath
+	if expected != actual {
+		t.Errorf("config mismatch: want %s; got %s", expected, actual)
 	}
 }
--- main_test.go
+++ main_test.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
+	"os"
 	"path"
 	"path/filepath"
 	"runtime"
@@ -8,6 +11,35 @@ import (
 
 func testModuleDir() string {
 	_, filename, _, _ := runtime.Caller(1)
-	p, _ := filepath.Abs(path.Join(path.Dir(filename), "test-modules"))
+	p, _ := filepath.Abs(path.Join(path.Dir(filename), "test/fixtures/modules"))
 	return p
+}
+
+func testModuleDirFor(module string) string {
+	return filepath.Join(testModuleDir(), module)
+}
+
+func setupFor(module string) {
+	_conf = nil // clear conf() cache
+	path := testModuleDirFor(module)
+	if err := os.Chdir(path); err != nil {
+		panic(err)
+	}
+}
+
+func execConfig(module string) bytes.Buffer {
+	setupFor(module)
+	var buf bytes.Buffer
+	stdout = &buf
+	commands["config"].Func()
+	stdout = os.Stdout
+	return buf
+}
+
+func execConfigParsed(module string) (c config) {
+	content := execConfig(module)
+	blob := content.Bytes()
+	err := json.Unmarshal(blob, &c)
+	check(err)
+	return
 }
